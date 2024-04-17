7bd4a5d2737d7108c2760b8d68f19e89363a48f8
pouria-moradi <pu.moradi@gmail.com>
2024-04-17 09:55:51 +0330

    Adding logger

---
+++ logger.py
@@ -0,0 +1,36 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+
+class Logger:
+    def __init__(self, logger_name, log_file, format:logging.Formatter=None) -> None:
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.DEBUG)
+
+        # Create a timed rotating file handler
+        file_handler = TimedRotatingFileHandler(log_file, when='midnight', interval=1, backupCount=30)  # Rotate daily, keep 30 days
+
+        # Set the logging level
+        file_handler.setLevel(logging.DEBUG)
+
+        # Create a formatter
+        if format:
+            formatter = format
+        else:
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%H:%M:%S')
+        file_handler.setFormatter(formatter)
+
+        # Add the file handler to the logger
+        logger.addHandler(file_handler)
+        self.logger = logger
+    
+    def debug(self, message):
+        self.logger.debug(message)
+    def info(self, message):
+        self.logger.info(message)
+    def error(self, message):
+        self.logger.error(message)
+
+if __name__ == "__main__":
+    logger = Logger('admin', './run.log')
+    logger.error('this is an error message')
