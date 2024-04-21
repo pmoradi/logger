3d0a33935648bc99c53bb8058f821a9ddf918e02
pouria moradi <32017107+pouria-moradi@users.noreply.github.com>
2024-04-21 17:37:03 +0330

    Update logger.py

--- logger/logger.py
+++ logger/logger.py
@@ -1,39 +1,34 @@
 import logging
-from logging.handlers import TimedRotatingFileHandler
+import json
+from logging.handlers import RotatingFileHandler
+
+class Json_Formatter(logging.Formatter):
+    def format(self, record):
+        log_record = {
+            'timestamp': self.formatTime(record),
+            'level': record.levelname,
+            'message': record.getMessage(),
+            'name': record.name,
+            'pathname': record.pathname,
+            'lineno': record.lineno,
+            'funcName': record.funcName
+        }
+        return json.dumps(log_record)
 
 class Logger:
-    def __init__(self, logger_name, log_file, format:logging.Formatter=None) -> None:
-        if self.get_logger(logger_name):
-            self.logger = self.get_logger(logger_name)
-            return
-        
-        logger = logging.getLogger(logger_name)
-        logger.setLevel(logging.DEBUG)
+    def __init__(self, name, log_file):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
 
-        # Create a timed rotating file handler
-        file_handler = TimedRotatingFileHandler(log_file, when='midnight', interval=1, backupCount=30)        # Set the logging level
+        file_handler = RotatingFileHandler(log_file, maxBytes=10485760, backupCount=5)
         file_handler.setLevel(logging.DEBUG)
 
-        # Create a formatter
-        if format:
-            formatter = format
-        else:
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', '%H:%M:%S')
-        file_handler.setFormatter(formatter)
+        # Create a JSON formatter
+        json_formatter = Json_Formatter()
 
-        # Add the file handler to the logger
-        logger.addHandler(file_handler)
-        self.logger = logger
-    
-    def debug(self, message):
-        self.logger.debug(message)
-    def info(self, message):
-        self.logger.info(message)
-    def error(self, message):
-        self.logger.error(message)
+        file_handler.setFormatter(json_formatter)
 
-    def get_logger(self, logger_name):
-        if logger_name in logging.Logger.manager.loggerDict:
-            logger_instance = logging.getLogger(logger_name)
-            return logger_instance
-        return None
+        self.logger.addHandler(file_handler)
+
+    def get_logger(self):
+        return self.logger
